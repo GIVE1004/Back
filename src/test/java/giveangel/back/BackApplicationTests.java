4c33be7774d88a0625545eff39e6c74e59e4749e
Dohun Lee <nestour95@gmail.com>
2023-10-14 21:17:59 +0900

    feat: 프로젝트 생성

---
+++ src/test/java/giveangel/back/BackApplicationTests.java
@@ -0,0 +1,13 @@
+package giveangel.back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
