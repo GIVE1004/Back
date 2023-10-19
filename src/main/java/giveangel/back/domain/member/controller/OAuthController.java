da495806e9cdd3b93c6051e07cb7756f9fb59ff0
이도훈 <nestour95@gmail.com>
2023-10-19 17:45:49 +0900

    chore: 자잘한 수정

--- src/main/java/giveangel/back/domain/member/controller/OAuthController.java
+++ src/main/java/giveangel/back/domain/member/controller/OAuthController.java
@@ -11,6 +11,7 @@ import lombok.SneakyThrows;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -47,7 +48,7 @@ public class OAuthController {
 		return ResponseEntity.ok().body(Message.success(result));
 	}
 
-	@GetMapping("/refresh")
+	@PostMapping("/refresh")
 	public ResponseEntity<Message<Tokens>> refreshToken(@RequestBody Tokens tokens) {
 		return ResponseEntity.ok().body(Message.success(oAuthService.refreshToken(tokens)));
 	}
