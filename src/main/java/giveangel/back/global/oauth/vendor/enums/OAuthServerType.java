0a0a071d644a76322338b8cdc11a8c9589979015
Dohun Lee <nestour95@gmail.com>
2023-10-30 21:21:09 +0900

    Revert "Develop/o auth2"

--- src/main/java/giveangel/back/global/oauth/vendor/enums/OAuthServerType.java
+++
@@ -1,9 +0,0 @@
-package giveangel.back.global.oauth.vendor.enums;
-
-public enum OAuthServerType {
-	KAKAO, NAVER, GOOGLE;
-
-	public static OAuthServerType fromName(String providerName) {
-		return OAuthServerType.valueOf(providerName.toUpperCase());
-	}
-}
